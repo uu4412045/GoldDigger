9113066457e7745c83569c99eff32f89b4adf219
Brett Wandel <u4412045@anu.edu.au>
2012-08-12 00:16:55 +1000

    Added the concurrency support as provided by the original server

--- src/com/golddigger/server/impl/GoldDiggerServlet.java
+++ src/com/golddigger/server/impl/GoldDiggerServlet.java
@@ -1,6 +1,8 @@
 package com.golddigger.server.impl;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.golddigger.server.GoldDiggerServer;
+import com.golddigger.server.Service;
 
 /**
  * The Servlet for the main interface for the Competitors. Simply passes the url to the {@link GoldDiggerServlet}
@@ -17,6 +20,7 @@ public class GoldDiggerServlet extends HttpServlet{
 	private static final long serialVersionUID = 1219399141770957347L;
 	private GoldDiggerServer server;
 	private DirectInputDelayedServer delayedServer;
+    private Set<String> executingSecrets = new HashSet<String>();
 	
 	public GoldDiggerServlet(GoldDiggerServer server){
 		super();
@@ -24,12 +28,43 @@ public class GoldDiggerServlet extends HttpServlet{
 	}
 	
 	@Override
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String url = req.getRequestURL().toString();
-		url = url.substring(url.indexOf("//")+2); // remove the "http://" from the url
-		
-		if (delayedServer != null) delayedServer.add(url);
-		server.process(url, resp.getWriter());
+		String sync = Service.parseURL(url, Service.URL_PLAYER);
+		if (sync == null) sync = "defaultSyncString";
+		try {
+			synchronized (executingSecrets) {
+                if(executingSecrets.contains(sync)) {
+                    resp.sendError(503);
+                    return;                        
+                } else {
+                   executingSecrets.add(sync);
+                }
+            }
+			synchronized(sync) {
+                long sleep = 100;
+                // long sleep = 20; // The sleep value is only used as a load limiter, no client should be able to overload the server. Change it if you want to test and is in a hurry
+                String header = req.getHeader("sleep");
+                if (header != null) {
+                    try {
+                        sleep = Long.parseLong(header);
+                    } catch (NumberFormatException e) {
+                        // never mind
+                    }
+                }
+                Thread.sleep(sleep);
+            }
+			if (delayedServer != null) delayedServer.add(url);
+			server.process(url, resp.getWriter());
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			executingSecrets.remove(sync);
+		}
+	}
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doPost(req,resp);
 	}
 
 	public void setDelayedServer(DirectInputDelayedServer delayedServer) {
---
+++ test/com/golddigger/TestConcurrency.java
@@ -0,0 +1,71 @@
+package com.golddigger;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import com.golddigger.model.Player;
+import com.golddigger.templates.TestGameTemplate;
+import com.meterware.httpunit.WebConversation;
+import com.meterware.httpunit.WebResponse;
+
+
+public class TestConcurrency {
+    private GenericServer server;
+	
+    @Before
+    public void startServer() throws Exception {
+    	server = new GenericServer();
+    }
+    
+    @After
+    public void stopServer() throws Exception {
+        server.stop();
+    }
+    @Test
+    public void testMove() throws Exception {   
+    	server.add(new TestGameTemplate("wwwww\nwmbmw\nwwwww"));
+    	server.add(new Player("test", "secret"));
+        Worker worker1 = new Worker("http://localhost:8066/golddigger/digger/test/move/east");
+        Worker worker2 = new Worker("http://localhost:8066/golddigger/digger/test/move/west");
+        Thread t1 = new Thread(worker1);
+        Thread t2 = new Thread(worker2);
+
+        t1.start();
+		Thread.sleep(200);
+        t2.start();
+        
+        while (t1.isAlive() || t2.isAlive()) Thread.sleep(100);
+        assertEquals("Sanity Check Failed",200, worker1.response.getResponseCode());
+        assertEquals("Did not error",503, worker2.response.getResponseCode());
+        
+    }
+    
+    private class Worker implements Runnable {
+    	String URL;
+    	public WebResponse response;
+    	public Worker(String URL){
+    		this.URL = URL;
+    	}
+		@Override
+		public void run() {
+	        WebConversation wc = new WebConversation();
+	        wc.setExceptionsThrownOnErrorStatus(false);
+	        try {
+				response = wc.getResponse(URL);
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (SAXException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
